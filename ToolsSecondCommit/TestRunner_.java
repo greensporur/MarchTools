0b9dc4282c848b5b8300eb4523096e5410a69f48
greensporur <mselvin2808@gmail.com>
2024-04-13 04:38:41 -0700

    Scnd

---
+++ ToolsSecondCommit/TestRunner_.java
@@ -0,0 +1,23 @@
+
+public class TestRunner_ {
+
+	
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\admin\\eclipse-workspace\\AdactinProcet\\Driver\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+}
